107dae281b050da3053e99b4c24688169536a471
iazis <peristiawanazis@gmail.com>
2016-04-04 22:16:07 +0700

    header color ok, background ok.

--- app/src/main/java/com/example/iazis/museum/homepage.java
+++ app/src/main/java/com/example/iazis/museum/homepage.java
@@ -1,13 +1,17 @@
 package com.example.iazis.museum;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class homepage extends AppCompatActivity {
+public class homepage extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
     }
 }
