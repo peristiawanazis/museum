687c6fb081bdfad36a28558a82a50987aebcd19e
iazis <peristiawanazis@gmail.com>
2016-04-05 23:03:29 +0700

    home logo, title are done.

--- app/src/main/java/com/example/iazis/museum/homepage.java
+++ app/src/main/java/com/example/iazis/museum/homepage.java
@@ -2,9 +2,11 @@ package com.example.iazis.museum;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class homepage extends Activity {
 
