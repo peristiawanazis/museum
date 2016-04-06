b0db609d07ff1990ef7aa8157a2f96fc1391771d
iazis <peristiawanazis@gmail.com>
2016-04-06 21:15:54 +0700

    list view form menu done

--- app/src/main/java/com/example/iazis/museum/homepage.java
+++ app/src/main/java/com/example/iazis/museum/homepage.java
@@ -6,14 +6,56 @@ import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class homepage extends Activity {
+
+    ListView list;
+    String[] web = {
+            "Google Plus",
+            "Twitter",
+            "Windows",
+            "Bing",
+            "Itunes",
+            "Wordpress",
+            "Drupal"
+    } ;
+    Integer[] imageId = {
+            R.drawable.ic_location_city_24dp,
+            R.drawable.ic_location_city_24dp,
+            R.drawable.ic_location_city_24dp,
+            R.drawable.ic_location_city_24dp,
+            R.drawable.ic_location_city_24dp,
+            R.drawable.ic_location_city_24dp,
+            R.drawable.ic_location_city_24dp
+
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        CustomList adapter = new
+                CustomList(homepage.this, web, imageId);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(homepage.this, "You Clicked at " + web[+position], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
 
     }
 }
